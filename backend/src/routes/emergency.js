5f5dc8e316c56ea6a43b168c36c81cd9a8a755bc
SeniorShield Deploy <deploy@seniorshield.app>
2026-04-05 07:30:38 -0400

    Clean: Remove all monorepo artifacts and old files

--- backend/src/routes/emergency.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const emergencyController = require("../controllers/emergencyController");
-const { requireAuth } = require("../middleware/auth");
-
-router.post("/sos", requireAuth, emergencyController.sos);
-router.post("/notify-family", requireAuth, emergencyController.notifyFamily);
-
-module.exports = router;
